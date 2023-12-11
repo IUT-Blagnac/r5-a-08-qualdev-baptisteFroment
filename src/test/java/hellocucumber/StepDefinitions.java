ea2ef5cbb5d2f39bf0874d57df86c9be2a1f074c
Froment Baptiste <baptiste.froment6@gmail.com>
2023-12-11 11:45:13 +0100

    Remove Cucumber test files and dependencies

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -24,21 +24,17 @@ public class StepDefinitions {
     public void i_should_be_told(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
     }
-    // Ajoutez ces méthodes à votre classe StepDefinitions
 
     @Given("an example scenario")
     public void an_example_scenario() {
-        // Vous pouvez laisser cette méthode vide si elle ne nécessite pas d'action spécifique
     }
 
     @When("all step definitions are implemented")
     public void all_step_definitions_are_implemented() {
-        // Vous pouvez laisser cette méthode vide si elle ne nécessite pas d'action spécifique
     }
 
     @Then("the scenario passes")
     public void the_scenario_passes() {
-        // Vous pouvez laisser cette méthode vide si elle ne nécessite pas d'action spécifique
     }
 
 }
